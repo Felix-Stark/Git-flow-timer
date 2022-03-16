5c3aad6010d3803c7380ba7b5102c698e9330def
JuKoNi <92727651+JuKoNi@users.noreply.github.com>
2022-03-16 20:12:44 +0100

    Update menu.js

--- JS/modules/menu.js
+++ JS/modules/menu.js
@@ -4,6 +4,8 @@ const visualTimerElem = document.querySelector('#visual');
 const textTimerElem = document.querySelector('#text');
 const circlesTimerElem = document.querySelector('#circles');
 
+import { digitalClock } from "../modules/digital.js"
+
 export function showMenuOptions () {
     analougeTimerElem.addEventListener('click', () => {
         
