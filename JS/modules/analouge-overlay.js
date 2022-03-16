22cff3bf0c6e668d9ae1db283078a730f75c7ce6
JuKoNi <92727651+JuKoNi@users.noreply.github.com>
2022-03-16 12:27:33 +0100

    osv

--- JS/modules/analouge-overlay.js
+++ JS/modules/analouge-overlay.js
@@ -1,30 +1,8 @@
 const mainElem = document.querySelector('main');
 
 
-function addClockToHTML () {
-
-    bodyElem.innerHTML = `
-
-
+export function addClockToHTML () {
     mainElem.innerHTML = `
-
-    <header class="flex menu__bar">
-        <nav>
-            <span class="material-icons material-icons-outlined">
-                menu
-            </span>
-            <section class="menu__overlay bg__color--333">
-                <ul class="menu__list flex column">  
-                    <li class="list__item">Analaog timer</li>
-                    <li class="list__item">Digital timer</li>
-                    <li class="list__item">Visual timer</li>
-                    <li class="list__item">Text timer</li>
-                    <li class="list__item">Circles timer</li>
-                </ul>
-            </section>
-        </nav>
-    </header>
-
     <main class="flex column">
         <div class="clock">
             <div class="dot"></div>
