7b85a4fa1cd987dae63bb85ab9216ebde3c8ba1a
Ida Larsson <77833853+IdaLarsson90@users.noreply.github.com>
2022-03-16 18:49:51 +0100

    påbörjat timerfunktion bland annat

--- JS/script.js
+++ JS/script.js
@@ -4,7 +4,8 @@ import { showMenuOptions } from "./modules/menu.js";
 
 const menuElem = document.querySelector('#menu');
 const openMenu = document.querySelector('#open-menu');
-const closeMenu = document.querySelector('#close-menu')
+const closeMenu = document.querySelector('#close-menu');
+
 
 showLoadingOverlay();
 
@@ -16,3 +17,34 @@ openMenu.addEventListener('click', ()=> {
 closeMenu.addEventListener('click', ()=> {
     menuElem.classList.toggle('show');
 });
+
+
+export function activateStartTimerButton (setTime){
+    const startBtn = document.querySelector('#start-btn');
+    
+    /* Fixa så att man inte kan starta timern om den redan är aktiv. Det fuckar upp */
+    startBtn.addEventListener('click', ()=> {
+       startTimer(setTime);
+    });
+}
+function startTimer (setTime){
+/* Som timertest bara. Just nu räknar den ner i konsolen som om man ställer in sekunder.
+Den behöver*/
+
+/* Vad är det han skrivit i general om easyTimer??? Är det något vi ska använda kanske? */
+    let currentTime = setTime;
+    let timer = setInterval(() => {
+        // Check if time left
+        if(currentTime >= 0) {
+           // Update timer in gui
+           console.log(currentTime);
+           // count down current time
+           currentTime--;
+       } else {
+        //    gameOver(timer, gameLoop)
+        console.log('times up')
+       }
+    }, 1000);
+}
+
+/* En stop timer kanske? */
