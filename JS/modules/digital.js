4b0f286b3f41c872e7a6166ddbffa9d7b308420d
Ida Larsson <77833853+IdaLarsson90@users.noreply.github.com>
2022-03-17 11:49:59 +0100

    easyTimer added

--- JS/modules/digital.js
+++ JS/modules/digital.js
@@ -3,9 +3,9 @@ const mainElem = document.querySelector('main');
 
 export function digitalClock(minutes, seconds) {
     mainElem.innerHTML = `
-        <div class="digital-timer">
-            <h1 class="minutes">${minutes}:</h1>
-            <h1 class="seconds">${seconds}</h1>
-        </div>
+    <div class="timerInfo">
+    
+    <h1 id="remaining">00</h1>
+</div> 
             <button class="button abort-btn">Abort timer</button>`;
 }
