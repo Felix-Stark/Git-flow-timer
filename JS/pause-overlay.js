c525db9a1ef9191f94add8c337b35da7102cd77c
Felix Stark <felix.b.stark@gmail.com>
2022-03-18 09:39:02 +0100

    Update pause-overlay.js

--- JS/pause-overlay.js
+++ JS/pause-overlay.js
@@ -13,3 +13,16 @@ const unPauseBtn = document.querySelector('.unpause-btn')
 const pauseTime = document.querySelector('#pause-time') //tiden när man trycker på pause
 
 
+function showPauseOverlay() {
+    mainElem.innerHTML = `
+    <section class="pause-overlay">
+                <div class="ring ring-one"></div>
+                <div class="ring ring-two"></div>
+                <div class="ring ring-three"></div>
+                <div class="ring ring-four"></div>
+                <img src="/assets/pause-icon.png" alt="pause-icon">
+                <h3>Pause & breathe</h3>
+                <p id="pause-time"></p>
+                <button class="button unpause-btn">NO PAUSE, GO NOW!</button>
+            </section>`
+}
