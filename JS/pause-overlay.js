c17b0d53c070024d987a70b54fbd3ec5167777a4
Felix Stark <felix.b.stark@gmail.com>
2022-03-18 06:29:39 +0100

    html-css klar

---
+++ JS/pause-overlay.js
@@ -0,0 +1,15 @@
+/** TODO
+ * visa overlay
+ * hämta variabeln som styr timern
+ * spara tiden som visas i timern när man trycker på pause
+ * visa pausetiden i overlay
+ * unpause knappen gör att tiden fortsätter där den pausades.
+ */
+
+
+const mainElem = document.querySelector('main')
+const pauseOverlay = document.querySelector('.pause-overlay')
+const unPauseBtn = document.querySelector('.unpause-btn')
+const pauseTime = document.querySelector('#pause-time') //tiden när man trycker på pause
+
+
